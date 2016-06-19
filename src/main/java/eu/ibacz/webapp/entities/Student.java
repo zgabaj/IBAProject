1de69ec24e7ed01c443a3d7ca85f2095637fd7c4
Marek Zgabaj <Marek Zgabaj>
2016-06-19 13:37:35 +0200

    add import for Pattern anotation

--- src/main/java/eu/ibacz/webapp/entities/Student.java
+++ src/main/java/eu/ibacz/webapp/entities/Student.java
@@ -4,6 +4,7 @@ package eu.ibacz.webapp.entities;
 import java.util.Date;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 
